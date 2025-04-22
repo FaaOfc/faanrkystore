3398096ffc68e18bb901550c99d3b58352bed382
FaaOfc <159248870+FaaOfc@users.noreply.github.com>
2025-04-23 01:00:22 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,48 @@
-pasang ke func dibawah
+const categoryDescriptions = {
+"Panel": "Panel Run Bot",
+"Jasa": "Jasa Yang Tersedia",
+"Topup": "Topup Game",
+"Instagram": "Suntik Instagram",
+"Tik Tok": "Suntik Tik Tok",
+};
+
+const categories = {
+"Panel": [
+{ name: "Tangan Kanan" , subname: "Panel",  price: 35000, image: "20250325_070407.jpg", desc: "BENEFIT\n\nDAPET OWN PANEL\nDAPET ADP\nDAPET 23 SERVER (PUBLIK)\nBISA OPEN ADP\nBISA OPEN RESELLER\nBISA OPEN OWN\nBISA JUAL PANEL\n\nSPEK? 16GB 4CORE" },
+{ name: "Tangan Kiri" , subname: "Panel",  price: 30000, image: "20250325_070407.jpg", desc: "BENEFIT\n\nDAPET OWN PANEL\nDAPET ADP\nDAPET 12 SERVER (PUBLIK)\nBISA OPEN ADP\nBISA OPEN RESELLER\nBISA OPEN OWN\nBISA JUAL PANEL\n\nSPEK? 16GB 4CORE" },
+{ name: "Owner" , subname: "Panel",  price: 2000, image: "20250325_070407.jpg", desc: "BENEFIT\n\nDAPET OWN PANEL\nDAPET ADP\nBISA OPEN ADP\nBISA OPEN RESELLER\nBISA JUAL PANEL\n\nSPEK? 16GB 4CORE" },
+{ name: "Admin" , subname: "Panel",  price: 10000, image: "20250325_070407.jpg", desc: "BENEFIT\n\nDAPET ADP\nBISA OPEN RESELLER\nBISA JUAL PANEL\n\nSPEK? 16GB 4CORE" },
+{ name: "Reseller Panel" , subname: "Privat",  price: 7000, image: "20250325_070407.jpg", desc: "BENEFIT\n\nDAPET RESELLER PANEL PRIVAT \nDAPET SC CPANEL\nBISA JUAL PANEL\n\nSPEK? 16GB 4CORE" },
+{ name: "Reseller Panel" , subname: "Publik",  price: 5000, image: "20250325_070407.jpg", desc: "BENEFIT\n\nDAPET RESELLER PANEL PUBLIK\nDAPET SC CPANEL\nBISA JUAL PANEL\n\nSPEK? 16GB 4CORE" },
+{ name: "Panel Bot" , subname: "Privat",  price: 10000, image: "20250325_070407.jpg", desc: "LIST PANEL PRIV\n\nR1G D1G C40% : 2000\nR2G D2G C60% : 3000\nR3G D3G C80% : 4000\nR4G D4G C100% : 5000\nR5G D4G C120% : 6000\nR6G D6G C140% : 7000\nR7G D7G C160% : 8000\nR8G D7G C180% : 9000\nR∞G D∞G C∞% : 10000\n\nNOTE\nR = RAM\nD = DISK\nC = CPU\nG = GB\n\nCONTOH:\nR1GB D1G C40%\nRAM 1GB\nDISK 1GB\nCPU 40%\n\nBENEFIT\n\nALWAYS ON\n GARANSI 10 HARI (1X)\nAMAN NO INTIP\nADMIN CUMA 1\n NO ADP" },
+{ name: "Panel Bot" , subname: "Publik",  price: 5000, image: "20250325_070407.jpg", desc: "LIST PANEL PUBLIK\n\nR1G D1G C40% : 1000\nR2G D2G C60% : 1500\nR3G D3G C80% : 2000\nR4G D4G C100% : 2500\nR5G D4G C120% : 3000\nR6G D6G C140% : 3500\nR7G D7G C160% : 4000\nR8G D7G C180% : 4500\nR∞G D∞G C∞% : 5000\n\nNOTE\nR = RAM\nD = DISK\nC = CPU\nG = GB\n\nCONTOH:\nR1GB D1G C40%\nRAM 1GB\nDISK 1GB\nCPU 40%\n\nBENEFIT\n\nALWAYS ON\n GARANSI 10 HARI (1X)\n\nNOTE:\nRESIKO INTIP SERVER\nADP LEBIH DARI 1\n GW JARANG CEK SERVER" }
+],
+"Jasa": [
+{ name: "Install Panel", subname: "Pterodactyl", price: 10000, image: "20250325_070407.jpg", desc: "Desk" },
+{ name: "Edit Script", subname: "Bot Wa", price: 10000, image: "20250325_070407.jpg", desc: "Desk" },
+{ name: "Edit Tampilan", subname: "LinkBio", price: 10000, image: "20250325_070407.jpg", desc: "Desk" }
+],
+"Tik Tok": [
+{ name: "TikTok", subname: "Views", price: 100, image: "20250325_070407.jpg", desc: "Rp. 100/1k Views\nMinimum Order Rp. 1.000" },
+{ name: "TikTok", subname: "Likes", price: 2000, image: "20250325_070407.jpg", desc: "Rp. 2.000/1k Likes" },
+{ name: "TikTok", subname: "Followers", price: 7000, image: "20250325_070407.jpg", desc: "Rp. 7.000/1k Followers" }
+],
+"Instagram": [
+{ name: "Instagram", subname: "Views", price: 50, image: "20250325_070407.jpg", desc: "Rp. 50/1k Views\nMinimum Order Rp. 1.000 " },
+{ name: "Instagram", subname: "Likes", price: 1000, image: "20250325_070407.jpg", desc: "Rp. 1.000/1k Likes" },
+{ name: "Instagram", subname: "Followers", price: 14000, image: "20250325_070407.jpg", desc: "Rp. 14.000/1k Followers" }
+],
+"Topup": [
+{ name: "Mobile Legend", subname: "Diamond", price: 2000, image: "20250325_070407.jpg", desc: "💎5 - Rp. 2.000\n💎12 - Rp. 5.000\n💎19 - Rp. 8.000\n💎28 - Rp. 9.000\n💎44 - Rp. 17.000\n💎50 - Rp. 15.000\n💎59 - Rp. 19.000\n💎86 - Rp. 23.000\n💎113 - Rp. 28.000\n💎144 - Rp. 38.000\n💎170 - Rp. 45.000\n💎239 - Rp. 56.000\n💎240 - Rp. 63.000\n💎257 - Rp. 68.000\n💎278 - Rp. 73.000\n💎284 - Rp. 75.000\n💎298 - Rp. 77.000\n💎301 - Rp. 79.000\n💎346 - Rp. 91.000\n💎355 - Rp. 93.000\n💎374 - Rp. 98.000\n💎408 - Rp. 106.000\n💎427 - Rp. 109.000\n💎450 - Rp. 118.000\n💎512 - Rp. 130.000\n💎622 - Rp. 143.000\n💎648 - Rp. 147.000\n💎720 - Rp. 164.000\n💎717 - Rp. 181.000\n💎790 - Rp. 192.000\n💎875 - Rp. 220.000\n💎896 - Rp. 226.000\n💎977 - Rp. 248.000" },
+{ name: "Mobile Legend", subname: "Membership", price: 28000, image: "20250325_070407.jpg", desc: "1X WDP - 💎220\n- Rp. 28.000 -\n2X WDP - 💎440\n- Rp. 55.000 -\n3X WDP - 💎660\n - Rp. 82.000 -\n4X WDP - 💎880\n- Rp. 109.000 -\n5X WDP - 💎1100\n- Rp. 135.000 -\nTwilight Pass\n- Rp. 145.000 -" },
+{ name: "Free Fire", subname: "Diamond", price: 2000, image: "20250325_070407.jpg", desc: "💎5 - Rp. 2.000\n💎10 - Rp. 3.000\n💎12 - Rp. 3.000\n💎50 - Rp. 8.000\n💎55 - Rp. 9.000\n💎70 - Rp. 9.000\n💎80 - Rp. 12.000\n💎100 - Rp. 14.000\n💎120 - Rp. 17.000\n💎130 - Rp. 18.000\n💎140 - Rp. 18.000\n💎145 - Rp. 19.000\n💎150 - Rp. 20.000\n💎190 - Rp. 25.000\n💎200 - Rp. 27.000\n💎210 - Rp. 27.000\n💎280 - Rp. 36.000\n💎355 - Rp. 45.000\n💎420 - Rp. 53.000\n💎500 - Rp. 63.000\n💎510 - Rp. 65.000\n💎565 - Rp. 71.000\n💎635 - Rp. 79.000\n💎720 - Rp. 86.000\n💎800 - Rp. 90.000\n💎860 - Rp. 91.000" },
+{ name: "Free Fire", subname: "Membership", price: 16000, image: "20250325_070407.jpg", desc: "Level Up Pass\n- Rp. 16.000 -\nMingguan Membership\n- Rp. 30.000 -\nBulanan Membership\n- Rp. 89.000 -\nBP Card\n- Rp. 44.000 -" },
+{ name: "Call Of Duty", subname: "CP", price: 5000, image: "20250325_070407.jpg", desc: "31 🪙\n- Rp. 5.000 -\n63 🪙\n- Rp. 10.000 -\n128 🪙\n- Rp. 19.000 -\n321 🪙\n- Rp. 46.000 -\n645 🪙\n- Rp. 96.000 -\n800 🪙\n- Rp. 108.000 -\n1373 🪙\n- Rp. 179.000 -\n2060 🪙\n- Rp. 268.000 -\n3564 🪙\n- Rp. 446.000 -\n5618 🪙\n- Rp. 651.000 -\n5619 🪙\n- Rp. 696.000 -\n7656 🪙\n- Rp. 891.000 -\n15312 🪙\n- Rp. 1.736.000 -\n38280 🪙\n- Rp. 4.339.000 -" },
+{ name: "PUBG Mobile", subname: "UC", price: 16000, image: "20250325_070407.jpg", desc: "60 UC 💴\n- Rp. 16.000 -\n300 + 25 UC 💴\n- Rp. 72.000 -\n600 + 60 UC 💴\n- Rp. 143.000 -\n1500 + 300 UC 💴\n- Rp. 358.000 -\n3000 + 850 UC 💴\n- Rp. 721.000 -\n6000 + 2100 UC 💴\n- Rp. 1.428.000 -\n-" },
+{ name: "Genshin Impact", subname: "Genesis Crystals", price: 12000, image: "20250325_070407.jpg", desc: "💎60\n- Rp. 12.000 -\n💎300 + 30\n- Rp. 60.000 -\n💎980 + 110\n- Rp. 179.000 -\n💎1280 + 140\n- Rp. 237.000 -\n💎1980 + 260\n- Rp. 386.000 -\n💎3280 + 600\n- Rp. 593.000 -\n💎4260 + 710\n- Rp. 770.000 -\n💎6480 + 1600\n- Rp. 1.185.000 -\n💎9760 + 2200\n- Rp. 1.776.000 -" },
+{ name: "Genshin Impact", subname: "Blessing Welkin", price: 60000, image: "20250325_070407.jpg", desc: "Blessing Welkin\nMoon\n1X - Rp. 60.000\n2X - Rp. 119.000\n3X - Rp. 178.000\n4X - Rp. 237.000\n5X - Rp. 296.000" }
+]
+};
 
 function renderProducts() {
 const container = document.getElementById("productContainer");
